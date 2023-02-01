08acac268b08318e19dcfa7ccfc223b449efb7a5
COdiwuor <cyrilondime@gmail.com>
2023-02-01 15:14:58 +0300

    Initial commit

---
+++ ASPNETCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ASPNETCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
